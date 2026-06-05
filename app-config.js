9a80fe5c745b27316a68f899a309c926eca4d093
Stellalris <163806238+Stellalris@users.noreply.github.com>
2026-06-06 00:37:04 +0800

    Add files via upload

---
+++ app-config.js
@@ -0,0 +1,6 @@
+window.APP_CONFIG = {
+  // Supabase 控制台 -> Settings -> API -> Project URL
+  SUPABASE_URL: "https://irctdrietcvpqgazsjwx.supabase.co",
+  // Supabase 控制台 -> Settings -> API -> anon public key
+  SUPABASE_ANON_KEY: "sb_publishable_xJbIk7-5N4iubEcBbc1x-g_AsB4jQQZ",
+};
